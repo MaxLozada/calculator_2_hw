db9c77cf0fc26e8cf92fbbf645c8046559877ff7
MaxLozada <56085654+MaxLozada@users.noreply.github.com>
2021-11-04 21:49:54 -0400

    adding final newline

--- calc/operations/calculations.py
+++ calc/operations/calculations.py
@@ -1,7 +1,8 @@
 """ Calculation class """
 
-class Calculation:
+class Calculation: # pylint: disable=too-few-public-methods
     """ Calculation class """
 
     def __init__(self, *args):
         self.values = args
+        
