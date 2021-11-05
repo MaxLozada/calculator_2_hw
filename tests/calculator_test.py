305e073fb43f89ae8c1d2aa24d0d6369cabb5618
MaxLozada <56085654+MaxLozada@users.noreply.github.com>
2021-11-04 21:57:11 -0400

    disabling unsued arguments

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -7,29 +7,34 @@ from calc.calculator import Calculator
 @pytest.fixture
 def clear_history_fixture():
     """define a function that will run each time you pass it to a test, it is called a fixture"""
-
+    # pylint: disable=redefined-outer-name
     Calculator.clear_history()
     return True
 
 def test_calculator_add(clear_history_fixture):
     """testing for addition method for calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
     assert Calculator.add_numbers(1.0, 2.0) == 3.0
 
 def test_calculator_subtract(clear_history_fixture):
     """Testing the subtract method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
     assert Calculator.subtract_numbers(1.0, 2.0) == -1.0
 
 def test_calculator_multiply(clear_history_fixture):
     """Testing the multiply method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
     assert Calculator.multiply_numbers(1.0, 2.0) == 2.0
 
 def test_calculator_division(clear_history_fixture):
     """Testing the division method of the calc"""
+    # pylint: disable=unused-argument,redefined-outer-name
     assert Calculator.divide_numbers(4, 2) == 2.0
     assert Calculator.divide_numbers(4, 0) == "No Zero in the Denominator"
 
 def test_clear_history(clear_history_fixture):
     """Testing clear history """
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.add_numbers(1.0,2.0)
     Calculator.clear_history()
@@ -37,6 +42,7 @@ def test_clear_history(clear_history_fixture):
 
 def test_remove_1st_calculation_history(clear_history_fixture):
     """Testing to remove first 1st calculation and make it an object"""
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.add_numbers(*(1, 2))
     Calculator.multiply_numbers(3, 3)
@@ -49,6 +55,7 @@ def test_remove_1st_calculation_history(clear_history_fixture):
 
 def test_remove_2nd_calculations_history(clear_history_fixture):
     """Testing to remove first 2nd calculation and make it an object"""
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.add_numbers(*(1, 5))
     Calculator.multiply_numbers(3, 4)
@@ -61,6 +68,7 @@ def test_remove_2nd_calculations_history(clear_history_fixture):
 
 def test_length_of_history(clear_history_fixture):
     """Testing the length of the history"""
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.add_numbers(*(2, 2))
     Calculator.add_numbers(3, 3)
@@ -70,6 +78,7 @@ def test_length_of_history(clear_history_fixture):
 
 def test_get_calculation(clear_history_fixture):
     """Testing getting a specific calculation out of the history"""
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.multiply_numbers(1.0,2.0)
     Calculator.add_numbers(3, 3, 3)
@@ -80,6 +89,7 @@ def test_get_calculation(clear_history_fixture):
 
 def test_get_calculation_last(clear_history_fixture):
     """Testing getting the last calculation from the history"""
+    # pylint: disable=unused-argument,redefined-outer-name
 
     Calculator.add_numbers(2, 4, 8)
     Calculator.add_numbers(3, 3, 3)
